0b77906577f2537c3212586f5fd5898f22ccd975
Alberto <dany.canaviri2022@gmail.com>
2025-08-27 23:29:20 -0400

    The initial structure of the project was created

---
+++ README.md
@@ -0,0 +1,8 @@
+# Shopping Cart with PHP 8.1
+
+### Shopping Cart with PHP 8.1
+
+- Shopping Cart, Using HTML CSS JavaScript AND PHP 8.1
+
+
+![preview img](/preview.png)
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="#" type="image/x-icon">
+
+        <!-- CSS - Bootstrap 5.3.8 - CDN -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <!-- CSS - FontAwesome 7.0.0 -->
+        <link rel="stylesheet" href="Util/fontawesome/css/all.min.css">
+        <!-- CSS - Toastr 2.1.4 - CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css">
+
+        <title>Carrito de Compras Online</title>
+    </head>
+
+    <body>
+        <h1>Carrito de Compras</h1>
+    </body>
+
+    <!-- JS - jQuery 3.7.1 - CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- JS - Bootstrap 5.3.8 - CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <!-- JS - Toastr 2.1.4 - CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/toastr.min.js"></script>
+    <!-- SweetAlert2 v11.22.5 - CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</html>
